77af93e4eb29428b8edc27457ffbefbff7297d6e
Mark William Schumacher <905mws@gmail.com>
2015-03-08 23:45:50 -0400

    Finished completely writing the Global / Local / Fitting Algorithms.

--- GlobalAndLocalAlignment.java
+++ GlobalAndLocalAlignment.java
@@ -1,44 +1,107 @@
-import java.util.Arrays;
+/*@Author Mark Schumacher
+ * This file contains the algorithms for local, global and fitting problems
+ * A description of these problems can be found on the readme
+ */
 
+import java.util.Arrays;
 
 public class GlobalAndLocalAlignment {
 	
 	static int [][] tester = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
 	
 	public static void main(String args[]){
-		String w = "GCATGCU";
-		String v = "GATTACA";
-		int[][] table = initializeTableforGlobal(w, v);
-		dynamicFillGlobal(table,w,v);
-		PrintTable(table,w,v);
-		System.out.println();
-		FindAlignment(table,w,v);
+		String w = "TAGATA";
+		String v = "GTAGGCTTAAGGTTA";
+		System.out.println("Fitting Alignment for \"TAGATA\" and \"GTAGGCTTAAGGTTA\"");
+		FittingAlignment(w,v, true, true);
+		
+		GlobalAlignment(w,v, false, true);
 		
 	  v = "ACACACTA";
 		w = "AGCACACA";
-		int[][] table2 = initializeTableforLocal(v, w);
-		dynamicFillLocal(table2,v,w);
-		PrintTable(table2,v,w);
+		LocalAlignment(v,w,true,true);
 	}
 	
-	public static String FindAlignment(int[][] table, String s1, String s2){
+	
+	public static String GlobalAlignment(String s1, String s2, 
+													boolean showTable, boolean showAlignment){
+		int[][] table = initializeTableforGlobal(s1, s2);
+		dynamicFillGlobal(table,s1,s2);
+		if (showTable==true){
+			PrintTable(table,s1,s2);
+		}
+		int rowCounter = table.length-1;
+		int colCounter = table[0].length-1;
+		String sDown = "" ,sAcross = "";
+		while (colCounter>0 && rowCounter>0){
+			int up = table[rowCounter-1][colCounter];
+			int left = table[rowCounter][colCounter-1];
+			int cur = table[rowCounter][colCounter];
+			if (cur == up-1){
+				sDown = String.valueOf(s2.charAt(rowCounter-1)).concat(sDown);
+				sAcross = "-".concat(sAcross);
+				rowCounter--;
+			}else if(cur == left-1){
+				sAcross = String.valueOf(s1.charAt(colCounter-1)).concat(sAcross);
+				sDown = "-".concat(sDown);
+				colCounter--;
+			}
+			else{
+				sAcross = String.valueOf(s1.charAt(colCounter-1)).concat(sAcross);
+				sDown = String.valueOf(s2.charAt(rowCounter-1)).concat(sDown);
+				colCounter--;
+				rowCounter--;
+			}
+		}
+		if (rowCounter==0 && colCounter>0){
+			String preFix= s1.substring(0, colCounter);
+			sAcross=preFix.concat(sAcross);
+			for (int i = 0; i< preFix.length(); i++){
+				sDown= "-".concat(sDown);
+			}
+		}
+		else if (colCounter==0 && rowCounter>0){
+			String preFix= s1.substring(0, rowCounter);
+			sDown=preFix.concat(sDown);
+			for (int i = 0; i< preFix.length(); i++){
+				sAcross= "-".concat(sAcross);
+			}
+		}
+		
+		if (showAlignment == true){
+			System.out.println("\nGlobal Alignment: ");
+			System.out.println(sAcross);
+			System.out.println(sDown);
+		}
+		return (sDown.concat("\n").concat(sAcross));
+	}
+	
+	
+	public static String FittingAlignment(String s1, String s2,
+														boolean showTable, boolean showAlignment){
+		/* This algorithm is not equivalent to global alignment. Here, we are
+		* considering all of s1 (Across word) and only a substring of s2 (Down word).
+		* This is not Global because both words have not been fully completed
+		* examined. We only care about s1 being completed
+		*/ 
+		int[][] table = initializeTableforGlobal(s1, s2);
+		dynamicFillGlobal(table,s1,s2);
+		if (showTable==true){
+			PrintTable(table,s1,s2);
+		}
+		
 		int maxRowIndex = 0;
 		for(int i=0; i<table.length; i++){
 			if (table[i][table[0].length-1]>= table[maxRowIndex][table[0].length-1])
 				maxRowIndex=i;
 		}
 		String sAcross= "";
-		String sDown= s2.substring(maxRowIndex);
-		for (int i = maxRowIndex; i< s2.length(); i++){
-			sAcross= "-".concat(sAcross);
-		}
-		
+		String sDown= "";
 		int colCounter=table[0].length-1;
 		int rowCounter=maxRowIndex;
 		while (colCounter>0 && rowCounter>0){
 			int up = table[rowCounter-1][colCounter];
 			int left = table[rowCounter][colCounter-1];
-			int diagonal = table[rowCounter-1][colCounter-1];
 			int cur = table[rowCounter][colCounter];
 			if (cur == up-1){
 				sDown = String.valueOf(s2.charAt(rowCounter-1)).concat(sDown);
@@ -57,37 +120,31 @@ public class GlobalAndLocalAlignment {
 			}
 		}
 		
-		if (colCounter==0 && rowCounter>0){
-			String preFix= s2.substring(0, rowCounter-1);
-			sDown=sDown.concat(preFix);
-			for (int i = 0; i< preFix.length(); i++){
-				sAcross= "-".concat(sAcross);
-			}
-		}
+		// Fill in remaining values after the boundary has been hit
+		// We dont do this for col ==0 because we only care to locally fit s1
 		if (rowCounter==0 && colCounter>0){
-			String preFix= s1.substring(0, colCounter-1);
-			sAcross=sAcross.concat(preFix);
+			String preFix= s1.substring(0, colCounter);
+			sAcross=preFix.concat(sAcross);
 			for (int i = 0; i< preFix.length(); i++){
 				sDown= "-".concat(sDown);
 			}
 		}
-			
-		System.out.println(sAcross);
-		System.out.println(sDown);
+		
+		if (showAlignment == true){
+			System.out.println("\nFitting:");
+			System.out.println(sAcross);
+			System.out.println(sDown);
+		}
 		return (sDown.concat("\n").concat(sAcross));
 		
 	}
 	
 	
-	public static int[][] initializeTableforLocal(String s, String s2){
-		int[][]table = new int[s.length()+1][s2.length()+1];
-		return table;		
-	}
-	
-	
-	public static int[][] dynamicFillLocal(int[][] table,String s1, String s2){
+	public static int[][] LocalAlignment(String s1, String s2, 
+			boolean showTable, boolean showAlignment){
+		int[][] table = new int[s2.length()+1][s1.length()+1];
 		for (int i= 1; i < table.length; i++){
-			for (int j = 1 ; j < table[0].length; j++){
+			for (int j = 1 ; j < table[i].length; j++){
 				int left = table[i-1][j]-1;
 				int up = table[i][j-1]-1;
 				int match = (s1.charAt(j-1) == s2.charAt(i-1))?2:-1;
@@ -95,13 +152,16 @@ public class GlobalAndLocalAlignment {
 				table[i][j]=Math.max(diagonal, Math.max(left, Math.max(up,0)));
 			}
 		}
+		if (showTable==true){
+			PrintTable(table, s1, s2);
+		}
+		
 		return table;
 	}
 	
-	
-	
+		
 	public static int[][] initializeTableforGlobal(String s, String s2){
-		int[][] table = new int[s.length()+1][];
+		int[][] table = new int[s2.length()+1][];
 		
 		// All zeros
 		for (int i= 0 ; i < table.length; i++){
@@ -118,6 +178,8 @@ public class GlobalAndLocalAlignment {
 		}	
 		return table;
 	}
+	
+	
 	public static int[][] dynamicFillGlobal(int[][] table,String s, String s2){
 		for (int i = 1 ; i< table.length; i++){
 			for (int j = 1; j< table[i].length; j++){
@@ -148,7 +210,7 @@ public class GlobalAndLocalAlignment {
 			}
 			System.out.println();
 		}
-		
+		System.out.println();
 	}
 	
 }
--- README.md
+++ README.md
@@ -4,9 +4,47 @@ Practice for technical interviews, done in preparation for onsite interviews dur
 These are technical interview prep questions taken from Elements of a Programming interview http://elementsofprogramminginterviews.com/pdf/epi-light-1.4.10.pdf and leetcode.com 
 
 
-## Local and Global Alignment Algorithms NEW*
+## Local and Global Alignment and Fitting Algorithms NEW*
 
 <img src="https://github.com/Mark-William-Schumacher/DataStructuresAndAlgorithms/blob/master/Images/Local-GlobalAlignment.PNG" height=250px > 
 
-GlobalAndLocalAlignment.java have some tester code written to solve some small scale dynamic programming local/global alignment problems. Algorythm is O(nm) space / time. 
-Todo - Can be done in O(n) space at not cost to time with a clever solution 
+GlobalAndLocalAlignment.java have some tester code written to solve some small scale dynamic programming local/global alignment problems. Algorithms are O(nm) space / time. 
+
+Example: 
+Solving Global alignment for s1= "TAGATA" , s2= "GTAGGCTTAAGGTTA"
+
+Global Alignment Matrix:
+    -    -    T    A    G    A    T    A
+    -    0   -1   -2   -3   -4   -5   -6
+    G   -1   -1   -2   -1   -2   -3   -4
+    T   -2    0   -1   -2   -2   -1   -2
+    A   -3   -1    1    0   -1   -2    0
+    G   -4   -2    0    2    1    0   -1
+    G   -5   -3   -1    1    1    0   -1
+    C   -6   -4   -2    0    0    0   -1
+    T   -7   -5   -3   -1   -1    1    0
+    T   -8   -6   -4   -2   -2    0    0
+    A   -9   -7   -5   -3   -1   -1    1
+    A  -10   -8   -6   -4   -2   -2    0
+    G  -11   -9   -7   -5   -3   -3   -1
+    G  -12  -10   -8   -6   -4   -4   -2
+    T  -13  -11   -9   -7   -5   -3   -3
+    T  -14  -12  -10   -8   -6   -4   -4
+    A  -15  -13  -11   -9   -7   -5   -3
+    
+Best Fitting:  O(n*m)
+Fitting is the problem of fully considering s1 for any substring in s2. 
+TAGA-T-A
+TAGGCTTA
+
+Global Alignment:  O(n*m)
+Both strings must be fully considered .
+GTAGGCTTAAGGTTA
+-TAG----A---T-A
+
+Local Alignment: O(n*m)
+Considering all substrings of s1 and s2.
+TAG
+TAG
+
+Can be done in O(n) space at not cost to time with a clever solution 
